475f5b58208c933041ae778a458f8c5fb9f471a5
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-14 17:50:17 +0900

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/lineageM/new/3_afterkey_PC_low.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
